277e1b05fe0cab668dd370a42c5afb28b266234c
akuidin <devour2035@gmail.com>
2026-05-13 17:11:19 +0300

    Add n+1 problem solving

--- src/main/java/com/winwintest/auth/repository/ProcessingLogRepository.java
+++ src/main/java/com/winwintest/auth/repository/ProcessingLogRepository.java
@@ -1,9 +1,18 @@
 package com.winwintest.auth.repository;
 
 import com.winwintest.auth.model.ProcessingLog;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProcessingLogRepository extends JpaRepository<ProcessingLog, Long> {
+
+	@Query("SELECT pl FROM ProcessingLog pl JOIN FETCH pl.user")
+	List<ProcessingLog> findAllWithUser();
+
+	@Query("SELECT pl FROM ProcessingLog pl JOIN FETCH pl.user WHERE pl.user.id = :userId")
+	List<ProcessingLog> findByUserIdWithUser(@Param("userId") Long userId);
 }
